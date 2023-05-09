b81926b8a0eefc6129378074fd541d1a83c3a2d1
Riham Mohamed <rihammohamed6121@gmail.com>
2023-05-10 01:08:07 +0300

    create new food- modified-

--- final_project/lib/Food/add_food_screen.dart
+++ final_project/lib/Food/add_food_screen.dart
@@ -1,68 +1,181 @@
 import 'package:final_project/constants.dart';
 import 'package:flutter/material.dart';
 
-class AddFoodScreen extends StatelessWidget {
+class AddFoodScreen extends StatefulWidget {
   AddFoodScreen({super.key});
   static const routeName = '/add-food';
+
+  @override
+  State<AddFoodScreen> createState() => _AddFoodScreenState();
+}
+
+class _AddFoodScreenState extends State<AddFoodScreen> {
   Map<String, dynamic> _foodData = {
     'name': '',
     'calories': 0,
     'fats': 0,
-    'protein': 0,
+    'proteins': 0,
     'carbs': 0,
     'imageUrl': ''
   };
+
   @override
   Widget build(BuildContext context) {
     final _deviceSize = MediaQuery.of(context).size;
     final _formKey = GlobalKey<FormState>();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Create new food'),
       ),
-      body: Stack(
-        children: [
-          Container(
-            width: _deviceSize.width,
-            height: _deviceSize.height,
-            decoration: BOX_DECORATION,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: _deviceSize.width,
-                height: _deviceSize.height * 0.3,
-                color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('Create new food',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 97, 219, 213),
+                  fontSize: 20,
+                  fontFamily: 'Anton',
+                  fontWeight: FontWeight.normal,
+                )),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.all(15),
+                width: 150,
+                height: 150,
+                color: Color.fromARGB(134, 158, 158, 158),
                 child: const Center(
                   child: Icon(Icons.add_a_photo_sharp),
                 ),
               ),
-              Form(
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              elevation: 8.0,
+              child: Container(
+                constraints: BoxConstraints(minWidth: _deviceSize.width * 0.7),
+                width: _deviceSize.width * 0.7,
+                padding: const EdgeInsets.all(20),
+                child: Form(
                   key: _formKey,
                   child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Text('Name: '),
-                          TextFormField(
-                            keyboardType: TextInputType.name,
-                            validator: (value) {
-                              if (value == null) {
-                                return 'Name field is required!';
-                              }
-                            },
-                            onSaved: (newValue) {
-                              _foodData['name'] = newValue;
-                            },
-                          )
-                        ],
-                      )
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: 'title'),
+                        keyboardType: TextInputType.name,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'name is required!';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _foodData['name'] = value;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: 'calories'),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'calories are required!';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _foodData['calories'] = value;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: 'fats'),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'fats are required!';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _foodData['fats'] = value;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: 'proteins'),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'proteins are required!';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _foodData['proteins'] = value;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: 'carbs'),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'carbs are required!';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _foodData['carbs'] = value;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 290,
+                        child: ElevatedButton(
+                          onPressed: () => {},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40)),
+                          ),
+                          child: const Text('Create',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
                     ],
-                  ))
-            ],
-          )
-        ],
+                  ),
+                ),
+              ),
+            )
+            /*Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            const Text('Name: '),
+                            TextFormField(
+                              keyboardType: TextInputType.name,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Name field is required!';
+                                }
+                              },
+                              onSaved: (newValue) {
+                                _foodData['name'] = newValue;
+                              },
+                            )
+                          ],
+                        )
+                      ],
+                    ))*/
+          ],
+        ),
       ),
     );
   }
--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.normal,
                 color: Colors.grey),
           )),
-      home: LogInScreen(),
+      home: AddFoodScreen(),
       routes: {
         OpenScreen.routeName: (context) => const OpenScreen(),
         LogInScreen.routeName: (context) => LogInScreen(),
