1effaa942df3b8295b557ed47091f49426c41404
Riham Mohamed <rihammohamed6121@gmail.com>
2023-05-15 23:11:36 +0300

    exercise model creation

---
+++ final_project/lib/models/exercise.dart
@@ -0,0 +1,16 @@
+class Execise {
+  final String id;
+  final String name;
+  final String bodypart;
+  final double caloriesBurnt;
+  final bool isRepetitive;
+  final String imageUrl;
+
+  Execise(
+      {required this.id,
+      required this.name,
+      required this.bodypart,
+      required this.caloriesBurnt,
+      required this.imageUrl,
+      required this.isRepetitive});
+}
