9004c1da042ee444fb1eb435acadc14355ba3c57
Youssef Rashed <youssef.rashed1999@gmail.com>
2023-05-23 21:29:00 +0300

    added Dialog to enter quantity of food eaten

--- final_project/lib/Food/diet_food_item.dart
+++ final_project/lib/Food/diet_food_item.dart
@@ -1,19 +1,22 @@
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../constants.dart';
 
 class DietFoodItem extends StatelessWidget {
   //const DietFoodItem({super.key});
 
-  final String id;
+  final int id;
   final String name;
-  final String imageUrl;
-  final double calories;
+  final String? imageUrl;
+  final int calories;
   final double fats;
   final double protein;
   final double carbs;
 
-  const DietFoodItem({
+  DietFoodItem({
+    super.key,
     required this.id,
     required this.name,
     required this.imageUrl,
@@ -22,23 +25,30 @@ class DietFoodItem extends StatelessWidget {
     required this.protein,
     required this.carbs,
   });
+  final GlobalKey<FormState> _formkey = GlobalKey();
+  double amount = 0;
+  void addFood() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? accessKey = prefs.getString(ACCESS_KEY);
+  }
 
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
+    //Rating Dialog
     void showRating() => showDialog(
           context: context,
           builder: (context) => AlertDialog(
-            title: Text('Rate this'),
+            title: const Text('Rate this'),
             content: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Text(
+                const Text(
                   'rate this food so we can recommend you something simmilar',
                   style: TextStyle(fontSize: 20),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
                 buildRating()
@@ -49,7 +59,7 @@ class DietFoodItem extends StatelessWidget {
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text(
+                  child: const Text(
                     'Ok',
                     style: TextStyle(fontSize: 20),
                   ))
@@ -57,6 +67,62 @@ class DietFoodItem extends StatelessWidget {
           ),
         );
 
+    void selectPortion() => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(
+              'Quantity',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).primaryColor),
+            ),
+            content: Form(
+              key: _formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Enter quantity',
+                    style: TextStyle(
+                        fontSize: 14, color: Theme.of(context).primaryColor),
+                  ),
+                  TextFormField(
+                    initialValue: 100.toString(),
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    validator: (value) {
+                      if (value!.isEmpty || double.parse(value) <= 0) {
+                        return 'Quantity can\'t be negative';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) => amount = double.parse(newValue!),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'Ok',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'Back',
+                    style: TextStyle(fontSize: 20),
+                  ))
+            ],
+          ),
+        );
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Material(
@@ -73,25 +139,32 @@ class DietFoodItem extends StatelessWidget {
               SizedBox(
                 width: 100,
                 height: 100,
-                child: Image.network(imageUrl, fit: BoxFit.fill),
+                child: imageUrl == null
+                    ? null
+                    : Image.network(imageUrl!, fit: BoxFit.fill),
               ),
               const SizedBox(
                 width: 7,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '$name\n',
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  Text('calories: $calories g'),
-                  Text('fats: $fats g'),
-                  Text('protien: $protein g'),
-                  Text('carbs: $carbs g'),
-                ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.29,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FittedBox(
+                      child: Text(
+                        '$name\n',
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text('calories: $calories g'),
+                    Text('fats: $fats g'),
+                    Text('protien: $protein g'),
+                    Text('carbs: $carbs g'),
+                  ],
+                ),
               ),
               const SizedBox(
                 width: 30,
@@ -101,7 +174,9 @@ class DietFoodItem extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      selectPortion();
+                    },
                     icon: const Icon(Icons.add_circle_outline_rounded),
                     iconSize: 40,
                     color: const Color.fromARGB(255, 97, 219, 213),
@@ -130,8 +205,8 @@ Widget buildRating() => RatingBar.builder(
       direction: Axis.horizontal,
       updateOnDrag: true,
       itemCount: 5,
-      itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
-      itemBuilder: (context, _) => Icon(
+      itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+      itemBuilder: (context, _) => const Icon(
         Icons.star,
         color: Colors.amber,
       ),
