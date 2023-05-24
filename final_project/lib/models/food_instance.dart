c51db40b05f87b395c1e50e9e63d1ff8ea31efb3
Youssef Rashed <youssef.rashed1999@gmail.com>
2023-05-24 18:00:26 +0300

    updated food instance class model

--- final_project/lib/models/food_instance.dart
+++ final_project/lib/models/food_instance.dart
@@ -4,5 +4,26 @@ class FoodInstance {
   int id;
   DietFood food;
   double quantity;
-  FoodInstance({required this.id, required this.food, required this.quantity});
+  int totalCalories;
+  double totalCarbs;
+  double totalFats;
+  double totalProtein;
+  FoodInstance(
+      {required this.id,
+      required this.food,
+      required this.quantity,
+      required this.totalCalories,
+      required this.totalCarbs,
+      required this.totalFats,
+      required this.totalProtein});
+  factory FoodInstance.fromjson(Map<String, dynamic> json) {
+    return FoodInstance(
+        id: json['id'],
+        food: DietFood.fromjson(json['food']),
+        quantity: json['quantity'],
+        totalCalories: json['total_calories'].toInt(),
+        totalCarbs: json['total_carbs'].toDouble(),
+        totalFats: json['total_fats'].toDouble(),
+        totalProtein: json['total_protein']);
+  }
 }
