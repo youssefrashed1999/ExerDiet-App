c270529e4966b4e51425f572a3b42f865f416d3f
Riham Mohamed <rihammohamed6121@gmail.com>
2023-05-25 03:58:32 +0300

    adding ingredient list attribute in recipe

--- final_project/lib/models/diet_recipe.dart
+++ final_project/lib/models/diet_recipe.dart
@@ -1,9 +1,12 @@
+import 'package:final_project/models/food_instance.dart';
+
 class DietRecipe {
   final String id;
   final String name;
+  final List<FoodInstance> ingredients;
   final String instructions;
   final String imageUrl;
 
-  DietRecipe(this.id, this.name, this.instructions, this.imageUrl);
-  
+  DietRecipe(
+      this.id, this.name, this.instructions, this.imageUrl, this.ingredients);
 }
