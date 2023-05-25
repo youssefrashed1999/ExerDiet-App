24b81cc11586de0138c38517d302418f89ffb804
Youssef Rashed <youssef.rashed1999@gmail.com>
2023-05-25 15:06:27 +0300

    Changed the look of food overview

--- final_project/lib/Food/food_overview_screen.dart
+++ final_project/lib/Food/food_overview_screen.dart
@@ -14,19 +14,15 @@ class FoodOverviewScreen extends StatefulWidget {
   State<FoodOverviewScreen> createState() => _FoodOverviewScreenState();
 }
 
-class _FoodOverviewScreenState extends State<FoodOverviewScreen> {
+class _FoodOverviewScreenState extends State<FoodOverviewScreen>
+    with SingleTickerProviderStateMixin {
   List<DietFood> loadedfood = List.empty(growable: true);
+  List<DietFood> loadedrecipe = List.empty(growable: true);
+  List<DietFood> loadedcustomfood = List.empty(growable: true);
   String? nextPage = "https://exerdiet.pythonanywhere.com/diet/foods/";
-  bool is_food_clicked = true;
-  // List<DietFood> displayloadedfood = List.from(loadedfood);
-  // void updateList(String value) {
-  //   setState(() {
-  //     displayloadedfood = loadedfood
-  //         .where((element) =>
-  //             element.name.toLowerCase().contains(value.toLowerCase()))
-  //         .toList();
-  //   });
-  // }
+  bool isFoodLoadingComplete = false;
+  bool isRecipeLoadingComplete = false;
+  bool isCustomFoodLoadingComplete = false;
   void getFood() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String? accessKey = prefs.getString(ACCESS_KEY);
@@ -46,150 +42,136 @@ class _FoodOverviewScreenState extends State<FoodOverviewScreen> {
           loadedfood
               .add(DietFood.fromjson(jsonDecode(response.body)['results'][i]));
         }
-        setState(() {});
+        setState(() {
+          isFoodLoadingComplete = true;
+        });
       }
     } catch (e) {
       print('problem is $e');
     }
   }
 
+  void getRecipe() async {}
+  void getCustomFood() async {}
+
   @override
   void initState() {
+    controller = TabController(length: 3, vsync: this);
     super.initState();
     getFood();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(is_food_clicked ? 'Add Food' : 'Add recipe')),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Text(is_food_clicked ? 'Add Food' : 'Add recipe',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Color.fromARGB(255, 97, 219, 213),
-                fontSize: 20,
-                fontFamily: 'Anton',
-                fontWeight: FontWeight.normal,
-              )),
-          const SizedBox(
-            height: 7,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.1),
-            child: TextField(
-              //onChanged: (value) => updateList(value),
-              style: const TextStyle(
-                  color: Color.fromARGB(255, 97, 219, 213), fontSize: 12),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color(0x00000000),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
-                hintText: "eg: Orange",
-                prefixIcon: const Icon(Icons.search),
+  Widget tabWidget(List foodList, bool isLoadingComplete, String value) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 7,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.1),
+          child: TextField(
+            //onChanged: (value) => updateList(value),
+            style: const TextStyle(
+                color: Color.fromARGB(255, 97, 219, 213), fontSize: 12),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color(0x00000000),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+              hintText: "eg: Orange",
+              suffixIcon: InkWell(
+                child: const Icon(Icons.search),
+                onTap: () {
+                  print('im here');
+                },
               ),
             ),
           ),
-          const SizedBox(
-            height: 7,
+        ),
+        if (isLoadingComplete == false)
+          const CircularProgressIndicator()
+        else if (isLoadingComplete == true && foodList.isNotEmpty)
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              itemCount:
+                  nextPage == null ? foodList.length : foodList.length + 1,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) {
+                if (nextPage != null && index == loadedfood.length) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        getFood();
+                      },
+                      child: const Text('Load more'));
+                } else {
+                  return DietFoodItem(
+                    id: loadedfood[index].id,
+                    name: loadedfood[index].name,
+                    imageUrl: loadedfood[index].imageUrl,
+                    calories: loadedfood[index].calories,
+                    fats: loadedfood[index].fats,
+                    protein: loadedfood[index].protein,
+                    carbs: loadedfood[index].carbs,
+                  );
+                }
+              },
+              scrollDirection: Axis.vertical,
+            ),
+          )
+        else
+          Center(child: Text('No $value found on Database!')),
+        if (value != 'Food')
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.horizontal(
+                          right: Radius.circular(40),
+                          left: Radius.circular(40))),
+                ),
+                onPressed: () {},
+                child: Text('create new $value',
+                    style: const TextStyle(color: Colors.white))),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: is_food_clicked
-                        ? const Color.fromARGB(255, 97, 219, 213)
-                        : Colors.white,
-                    elevation: is_food_clicked ? 4 : 2,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.horizontal(
-                            right: Radius.zero, left: Radius.circular(40))),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      is_food_clicked = true;
-                    });
-                  },
-                  child: Text('Food',
-                      style: TextStyle(
-                          color: is_food_clicked
-                              ? Colors.white
-                              : const Color.fromARGB(255, 97, 219, 213)))),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: is_food_clicked
-                        ? const Color.fromARGB(255, 255, 255, 255)
-                        : const Color.fromARGB(255, 97, 219, 213),
-                    elevation: is_food_clicked ? 2 : 4,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.horizontal(
-                            left: Radius.zero, right: Radius.circular(40))),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      is_food_clicked = false;
-                    });
-                  },
-                  child: Text('Recipes',
-                      style: TextStyle(
-                          color: is_food_clicked
-                              ? const Color.fromARGB(255, 97, 219, 213)
-                              : Colors.white)))
-            ],
-          ),
-          if (loadedfood.isNotEmpty)
-            Expanded(
-              child: ListView.builder(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                itemCount: nextPage == null
-                    ? loadedfood.length
-                    : loadedfood.length + 1,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  if (nextPage!=null &&index == loadedfood.length) {
-                    return ElevatedButton(
-                        onPressed: () {
-                          getFood();
-                        },
-                        child: const Text('Load more'));
-                  } else {
-                    return DietFoodItem(
-                      id: loadedfood[index].id,
-                      name: loadedfood[index].name,
-                      imageUrl: loadedfood[index].imageUrl,
-                      calories: loadedfood[index].calories,
-                      fats: loadedfood[index].fats,
-                      protein: loadedfood[index].protein,
-                      carbs: loadedfood[index].carbs,
-                    );
-                  }
-                },
-                scrollDirection: Axis.vertical,
-              ),
-            )
-          else
-            const CircularProgressIndicator(),
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 4,
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.horizontal(
-                        right: Radius.circular(40), left: Radius.circular(40))),
-              ),
-              onPressed: () {},
-              child: Text(
-                  is_food_clicked ? 'create new food' : 'create new recipe',
-                  style: const TextStyle(color: Colors.white))),
-        ],
+      ],
+    );
+  }
+
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Food'),
+        bottom: TabBar(
+            controller: controller,
+            unselectedLabelStyle: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Colors.black, fontWeight: FontWeight.normal),
+            unselectedLabelColor: Colors.black,
+            labelColor: Colors.white,
+            labelStyle: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+            tabs: const [
+              Padding(padding: EdgeInsets.only(bottom: 2), child: Text('Food')),
+              Padding(
+                  padding: EdgeInsets.only(bottom: 2), child: Text('Recipe')),
+              Padding(
+                  padding: EdgeInsets.only(bottom: 2), child: Text('My Food')),
+            ]),
       ),
+      body: TabBarView(controller: controller, children: [
+        tabWidget(loadedfood, isFoodLoadingComplete, 'Food'),
+        tabWidget(loadedrecipe, isRecipeLoadingComplete, 'Recipe'),
+        tabWidget(loadedcustomfood, isCustomFoodLoadingComplete, 'Custom Food'),
+      ]),
     );
   }
 }
