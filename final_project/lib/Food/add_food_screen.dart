57b61f6053799b4b83585b29cd990dd146878df2
Riham Mohamed <rihammohamed6121@gmail.com>
2023-05-26 22:15:16 +0300

    adding image from gallery or camera

--- final_project/lib/Food/add_food_screen.dart
+++ final_project/lib/Food/add_food_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:final_project/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddFoodScreen extends StatefulWidget {
   AddFoodScreen({super.key});
@@ -19,8 +21,9 @@ class _AddFoodScreenState extends State<AddFoodScreen> {
     'carbs': 0,
     'imageUrl': ''
   };
-  String selectedCategory = 'F';
   final _formKey = GlobalKey<FormState>();
+  //the function that determines the measurement unit based on food category//
+  String selectedCategory = 'F';
   String measurmentPicker() {
     if (selectedCategory.contains('F')) {
       return '/100 gm';
@@ -29,6 +32,53 @@ class _AddFoodScreenState extends State<AddFoodScreen> {
     } else {
       return '/1 spoon';
     }
+  }
+
+  //.......................................................//
+  var _myimage;
+  _showOption(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Upload photo from',
+                  style: TextStyle(color: Colors.black)),
+              content: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.image),
+                      title: Text('Gallery'),
+                      onTap: () {
+                        _UploadFromGallery(context);
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.camera),
+                      title: Text('Camera'),
+                      onTap: () {
+                        _UploadFromCamera(context);
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ));
+  }
+
+  Future _UploadFromGallery(BuildContext context) async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      _myimage = File(image!.path);
+    });
+    Navigator.pop(context);
+  }
+
+  Future _UploadFromCamera(BuildContext context) async {
+    var image = await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      _myimage = File(image!.path);
+    });
+    Navigator.pop(context);
   }
 
   @override
@@ -55,16 +105,32 @@ class _AddFoodScreenState extends State<AddFoodScreen> {
                   fontWeight: FontWeight.normal,
                 )),
             Center(
-              child: Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                width: 150,
-                height: 150,
-                color: Color.fromARGB(134, 158, 158, 158),
-                child: const Center(
-                  child: Icon(Icons.add_a_photo_sharp),
-                ),
-              ),
+              child: _myimage != null
+                  ? Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      width: 150,
+                      height: 150,
+                      child: Image.file(
+                        _myimage,
+                        width: 150,
+                        height: 150,
+                      ),
+                    )
+                  : Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      width: 150,
+                      height: 150,
+                      color: const Color.fromARGB(134, 158, 158, 158),
+                      child: Center(
+                        child: IconButton(
+                            onPressed: () {
+                              _showOption(context);
+                            },
+                            icon: const Icon(Icons.camera_alt_rounded)),
+                      ),
+                    ),
             ),
             Center(
               child: DropdownButton<String>(
@@ -80,7 +146,7 @@ class _AddFoodScreenState extends State<AddFoodScreen> {
                           : value.contains('B')
                               ? 'beverage'
                               : 'seasoning',
-                      style: TextStyle(
+                      style: const TextStyle(
                           fontSize: 16, color: Color.fromARGB(136, 0, 0, 0)),
                     ),
                   );
