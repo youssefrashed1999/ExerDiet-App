af6dde6f42ad23b9a3b536eba4f5216248eb3291
Youssef Rashed <youssef.rashed1999@gmail.com>
2023-05-28 01:16:21 +0300

    Updated diet recipe class model

--- final_project/lib/models/diet_recipe.dart
+++ final_project/lib/models/diet_recipe.dart
@@ -1,12 +1,40 @@
 import 'package:final_project/models/food_instance.dart';
 
 class DietRecipe {
-  final String id;
+  final int id;
   final String name;
   final List<FoodInstance> ingredients;
-  final String instructions;
-  final String imageUrl;
+  final String? instructions;
+  final String? imageUrl;
+  final int totalCalories;
+  final double totalCarbs;
+  final double totalProtein;
+  final double totalFats;
 
   DietRecipe(
-      this.id, this.name, this.instructions, this.imageUrl, this.ingredients);
+      {required this.id,
+      required this.name,
+      required this.instructions,
+      required this.imageUrl,
+      required this.ingredients,
+      required this.totalCalories,
+      required this.totalCarbs,
+      required this.totalFats,
+      required this.totalProtein});
+  factory DietRecipe.fromjson(Map<String, dynamic> json) {
+    List<FoodInstance> dummyList = List.empty(growable: true);
+    for (int i = 0; i < json['food_instances'].length; i++) {
+      dummyList.add(FoodInstance.fromjson(json['food_instances'][i]));
+    }
+    return DietRecipe(
+        id: json['id'],
+        name: json['name'],
+        instructions: json['instructions'],
+        imageUrl: json['image'],
+        ingredients: dummyList,
+        totalCalories: json['total_calories'].toInt(),
+        totalCarbs: json['total_carbs'].toDouble(),
+        totalFats: json['total_fats'].toDouble(),
+        totalProtein: json['total_protein'].toDouble());
+  }
 }
