73b08d8640152ebf527f9b546975399d6b12ae05
Youssef Rashed <youssef.rashed1999@gmail.com>
2023-05-28 01:15:46 +0300

    Updates meal class model and added a contructor to convert from json to Meal object

--- final_project/lib/models/meal.dart
+++ final_project/lib/models/meal.dart
@@ -22,4 +22,25 @@ class Meal {
     required this.totalFats,
     required this.totalProtein,
   });
+  factory Meal.fromjson(Map<String, dynamic> json) {
+    List<DietRecipe> recipeList = List.empty(growable: true);
+    List<FoodInstance> foodList = List.empty(growable: true);
+    for (int i = 0; i < json['food_instances'].length; i++) {
+      foodList.add(FoodInstance.fromjson(json['food_instances'][i]));
+    }
+    for (int i = 0; i < json['recipes'].length; i++) {
+      recipeList.add(DietRecipe.fromjson(json['recipes'][i]));
+    }
+    return Meal(
+      id: json['id'],
+      name: json['name'],
+      timeEaten: json['time_eaten'].toString(),
+      recipes: recipeList,
+      food: foodList,
+      totalCalories: json['total_calories'].toInt(),
+      totalCarbs: json['total_carbs'].toDouble(),
+      totalFats: json['total_fats'].toDouble(),
+      totalProtein: json['total_protein'].toDouble()
+    );
+  }
 }
