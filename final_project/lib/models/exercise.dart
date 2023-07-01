525aed6c9b045b4e3c8dd0830c4d575ef82da93f
Riham Mohamed <rihammohamed6121@gmail.com>
2023-07-01 05:26:12 +0300

    exercise model created

--- final_project/lib/models/exercise.dart
+++ final_project/lib/models/exercise.dart
@@ -4,7 +4,8 @@ class Execise {
   final String bodypart;
   final double caloriesBurnt;
   final bool isRepetitive;
-  final String imageUrl;
+  final String? imageUrl;
+  final int rate;
 
   Execise(
       {required this.id,
@@ -12,5 +13,17 @@ class Execise {
       required this.bodypart,
       required this.caloriesBurnt,
       required this.imageUrl,
-      required this.isRepetitive});
+      required this.isRepetitive,
+      this.rate = 0});
+
+  factory Execise.fromjson(Map<String, dynamic> json) {
+    return Execise(
+      id: json['id'],
+      name: json['name'],
+      caloriesBurnt: json['calories_burned'].toInt(),
+      bodypart: json['body_part'],
+      isRepetitive: json['is_repetitive'],
+      imageUrl: json['image'],
+    );
+  }
 }
