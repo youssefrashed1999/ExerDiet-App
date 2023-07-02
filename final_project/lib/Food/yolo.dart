7e891ac7ca33a0ff4efd21e46c532b33bf3992b7
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2023-07-02 22:19:34 +0300

    yolo added

---
+++ final_project/lib/Food/yolo.dart
@@ -0,0 +1,243 @@
+import 'dart:ui';
+
+import 'package:camera/camera.dart';
+import 'package:final_project/Food/yolo_submit_food.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_vision/flutter_vision.dart';
+
+enum Options { none, image, frame, vision }
+
+late List<CameraDescription> cameras;
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  runApp(
+    const MaterialApp(
+      home: Yolo(),
+    ),
+  );
+}
+
+class Yolo extends StatefulWidget {
+  static const routeName = '/yolo';
+  const Yolo({Key? key}) : super(key: key);
+
+  @override
+  State<Yolo> createState() => _YoloState();
+}
+
+class _YoloState extends State<Yolo> {
+  late int mealId;
+  Options option = Options.none;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    mealId = ModalRoute.of(context)!.settings.arguments as int;
+    return Scaffold(body: YoloVideo(mealId: mealId,));
+  }
+
+  Widget task(Options option) {
+    return YoloVideo(mealId: mealId,);
+  }
+}
+
+class YoloVideo extends StatefulWidget {
+  int mealId;
+  YoloVideo({Key? key,required this.mealId}) : super(key: key);
+
+  @override
+  State<YoloVideo> createState() => _YoloVideoState();
+}
+
+class _YoloVideoState extends State<YoloVideo> {
+  late CameraController controller;
+  late FlutterVision vision;
+  late List<Map<String, dynamic>> yoloResults;
+  CameraImage? cameraImage;
+  bool isLoaded = false;
+  bool isDetecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  init() async {
+    cameras = await availableCameras();
+    vision = FlutterVision();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((value) {
+      loadYoloModel().then((value) {
+        setState(() {
+          isLoaded = true;
+          isDetecting = false;
+          yoloResults = [];
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    controller.dispose();
+    await vision.closeYoloModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    if (!isLoaded) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Please Wait..."),
+        ),
+      );
+    }
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: CameraPreview(
+            controller,
+          ),
+        ),
+        ...displayBoxesAroundRecognizedObjects(size),
+        Positioned(
+          bottom: 75,
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                  width: 5, color: Colors.white, style: BorderStyle.solid),
+            ),
+            child: isDetecting
+                ? IconButton(
+                    onPressed: () async {
+                      stopDetection();
+                    },
+                    icon: const Icon(
+                      Icons.stop,
+                      color: Colors.red,
+                    ),
+                    iconSize: 50,
+                  )
+                : IconButton(
+                    onPressed: () async {
+                      await startDetection();
+                    },
+                    icon: const Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                    iconSize: 50,
+                  ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> loadYoloModel() async {
+    await vision.loadYoloModel(
+        labels: 'assets/yolo/yolov8n.txt',
+        modelPath: 'assets/yolo/yolov8n.tflite',
+        modelVersion: "yolov8",
+        numThreads: 10,
+        useGpu: true);
+    setState(() {
+      isLoaded = true;
+    });
+  }
+
+  Future<void> yoloOnFrame(CameraImage cameraImage) async {
+    final result = await vision.yoloOnFrame(
+        bytesList: cameraImage.planes.map((plane) => plane.bytes).toList(),
+        imageHeight: cameraImage.height,
+        imageWidth: cameraImage.width,
+        iouThreshold: 0.5,
+        confThreshold: 0.6,
+        classThreshold: 0.6);
+    if (result.isNotEmpty) {
+      setState(() {
+        yoloResults = result;
+      });
+    }
+  }
+
+  Future<void> startDetection() async {
+    setState(() {
+      isDetecting = true;
+    });
+    if (controller.value.isStreamingImages) {
+      return;
+    }
+    await controller.startImageStream((image) async {
+      if (isDetecting) {
+        cameraImage = image;
+        yoloOnFrame(image);
+      }
+    });
+  }
+
+  Future<void> stopDetection() async {
+    setState(() {
+      isDetecting = false;
+      // showDialog(
+      //     context: context,
+      //     builder: ((context) => AlertDialog(
+      //           title: const Text("Results"),
+      //           content: Text(yoloResults.toString()),
+      //         ))).then((value) {
+      //   yoloResults.clear();
+      // });
+      showModalBottomSheet(
+          context: context,
+          builder: (context) => YoloSubmitFood(
+                results: yoloResults.toList(),
+                mealId: widget.mealId,
+              ));
+    });
+  }
+
+  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
+    if (yoloResults.isEmpty) return [];
+    double factorX = screen.width / (cameraImage?.height ?? 1);
+    double factorY = screen.height / (cameraImage?.width ?? 1);
+
+    Color colorPick = const Color.fromARGB(255, 50, 233, 30);
+
+    return yoloResults.map((result) {
+      return Positioned(
+        left: result["box"][0] * factorX,
+        top: result["box"][1] * factorY,
+        width: (result["box"][2] - result["box"][0]) * factorX,
+        height: (result["box"][3] - result["box"][1]) * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(color: Colors.pink, width: 2.0),
+          ),
+          child: Text(
+            "${result['tag']} ${(result['box'][4] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = colorPick,
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+}
