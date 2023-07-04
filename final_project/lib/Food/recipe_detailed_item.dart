6d911dff2c3cdf92e6dd3b7355c4a850ba15b028
Riham Mohamed <rihammohamed6121@gmail.com>
2023-07-04 18:59:55 +0300

    Update recipe_detailed_item.dart

--- final_project/lib/Food/recipe_detailed_item.dart
+++ final_project/lib/Food/recipe_detailed_item.dart
@@ -1,8 +1,6 @@
-
 import 'package:final_project/constants.dart';
 import 'package:final_project/home/meals/food_instance_item.dart';
 import 'package:flutter/material.dart';
-
 
 import '../models/diet_recipe.dart';
 
@@ -18,154 +16,87 @@ class RecipeDetailedItem extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(recipe.name)),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: deviceSize.height * 0.2,
-              child: recipe.imageUrl != null
-                  ? Image.network(
-                      '$BASE_URL${recipe.imageUrl}',
-                      fit: BoxFit.fitHeight,
-                    )
-                  : null,
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
+        child: Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: Text('Name',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 16)),
+              SizedBox(
+                width: double.infinity,
+                height: deviceSize.height * 0.2,
+                child: recipe.imageUrl != null
+                    ? Image.network(
+                        '$BASE_URL${recipe.imageUrl}',
+                        fit: BoxFit.fitHeight,
+                      )
+                    : null,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                recipe.name,
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
+              Card(
+                margin: EdgeInsets.only(bottom: 10),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                elevation: 8.0,
+                child: Container(
+                  constraints: BoxConstraints(minWidth: deviceSize.width * 0.8),
+                  width: deviceSize.width * 0.8,
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Name:',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(recipe.name),
+                      const Divider(
+                        color: Color.fromARGB(255, 213, 211, 211),
+                        height: 2,
+                        thickness: 1,
+                        indent: 0,
+                        endIndent: 5,
+                      ),
+                      Text(
+                        'Instructions:',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(recipe.instructions!),
+                      const Divider(
+                        color: Color.fromARGB(255, 213, 211, 211),
+                        height: 2,
+                        thickness: 1,
+                        indent: 0,
+                        endIndent: 5,
+                      ),
+                      Text('Nutritional Facts:',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('Total calories: ${recipe.totalCalories} cal'),
+                      Text('Total Protein: ${recipe.totalProtein} g'),
+                      Text('Total Carbs: ${recipe.totalCarbs} g'),
+                      Text('Total Fats: ${recipe.totalFats} g')
+                    ],
+                  ),
+                ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: Text('Instructions',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 16)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                recipe.instructions!,
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: Text('Nutrition Facts',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 16)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                'Total calories: ${recipe.totalCalories} cal',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                'Total Protein: ${recipe.totalProtein} g',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                'Total Carbs: ${recipe.totalCarbs} g',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Text(
-                'Total Fats: ${recipe.totalFats} g',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 14),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5),
+              Container(
                 child: Text('Ingredients',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 16)),
+                    style: TextStyle(fontWeight: FontWeight.bold)),
               ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: recipe.ingredients.length,
-              itemBuilder: (BuildContext context, int index) =>
-                  FoodInstanceItem(food: recipe.ingredients[index]),
-              scrollDirection: Axis.vertical,
-            ),
-          ],
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    FoodInstanceItem(food: recipe.ingredients[index]),
+                scrollDirection: Axis.vertical,
+              ),
+            ],
+          ),
         ),
       ),
     );
